f12c24fd941dc384566127d13ee7d56b0f16f02a
Satender sharma <satender80010336@gmail.com>
2023-06-23 00:21:00 +0530

    Project complete

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WebStoriesIcon from '@mui/icons-material/WebStories';
+
+function Header() {
+  return (
+    <header>
+      <h1><WebStoriesIcon />Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
