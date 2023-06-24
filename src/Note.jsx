8cdbd7c25a281e651ed8d4fd29c89965bdb6100a
Satender sharma <satender80010336@gmail.com>
2023-06-24 15:03:09 +0530

    minor bug fixes

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
+import Fab from '@mui/material/Fab';
 function Note(props) {
   function handleClick() {
     props.onDelete(props.id);
@@ -9,7 +10,7 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}><DeleteIcon /></button>
+      <Fab onClick={handleClick}><DeleteIcon /></Fab>
     </div>
   );
 }
